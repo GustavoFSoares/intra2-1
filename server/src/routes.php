f283bdb369796e139eb57d074cbe7957c30cad2e
Gustavo Soares <gustavo10.fsoares@gmail.com>
2018-04-13 14:41:04 -0300

    criando rotas para links

--- server/src/routes.php
+++ server/src/routes.php
@@ -12,3 +12,24 @@ $app->get('/[{name}]', function (Request $request, Response $response, array $ar
     // Render index view
     return $this->renderer->render($response, 'index.phtml', $args);
 });
+
+$app->get('/link/{id}', function(Request $req, Response $res, array $args) {
+    $id = $args['id'];
+    $this->logger->info("Get link by id: $id");
+
+    $res->getBody()->write(json_encode(['links' => true]));
+
+    return $res;
+});
+
+$app->get('/links', function(Request $req, Response $res, array $args) {
+    $this->logger->info("Get all links");
+
+    $res = $res->withHeader('Content-Type', 'application/json');
+    $res = $res->withHeader('Access-Control-Allow-Origin', '*');
+    $res = $res->withHeader('Access-Control-Allow-Methods', '*');
+    $res = $res->withHeader('Access-Control-Allow-Headers', '*');
+    
+    $res->getBody()->write(json_encode(['links' => true]));
+    return $res;
+});
