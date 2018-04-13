e61b8ac1e863fd39d0fa9bc37fb276c3fa33d357
Gustavo Soares <gustavo10.fsoares@gmail.com>
2018-04-13 14:42:09 -0300

    implementado pojo para links

---
+++ server/src/HospitalApi/Entity/Link.php
@@ -0,0 +1,88 @@
+<?php
+namespace HospitalApi\Entity;
+
+use HospitalApi\Entity\EntityAbstract;
+
+/**
+ * @Entity
+ * @Table(name="link")
+ */
+class Link extends EntityAbstract
+{
+    
+        /**
+     * @var integer @Id
+     *     @Column(name="id", type="integer")
+     *     @GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+    // public $id;
+
+    /**
+     * @var string @Column(type="string", length=255)
+     */
+    protected $url;
+    // public $url;
+
+    /**
+     * @var string @Column(type="string", length=255)
+     */
+    protected $title;
+    // public $title;
+
+    /**
+     * @var string @Column(type="string", length=255)
+     */
+    protected $icon;
+    // public $icon;
+
+    /**
+     * @var boolean @Column(type="boolean")
+     */
+    protected $externalLink;
+    // public $externalLink;
+
+    public function __contruct($url = '', $title = '', $icon = '', $externalLink = false){
+        $this->id = 0;
+        $this->url = $url;
+        $this->title = $title;
+        $this->icon = $icon;
+        $this->externalLink = $externalLink;
+    }
+
+    public function getId(){
+        return $this->id;
+    }
+    public function setId($id){
+        $this->id = $id;
+    }
+    
+    public function getUrl(){
+        return $this->url;
+    }
+    public function setUrl($url){
+        $this->url = $url;
+    }
+
+    public function getTitle(){
+        return $this->title;
+    }
+    public function setTitle($title){
+        $this->title = $title;
+    }
+
+    public function getIcon(){
+        return $this->icon;
+    }
+    public function setIcon($icon){
+        $this->icon = $icon;
+    }
+
+    public function isExternalLink(){
+        return $this->externalLink;
+    }
+    public function setExternalLink($externalLink){
+        $this->externalLink = $externalLink;
+    }
+
+}
