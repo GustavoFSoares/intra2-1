ab80e4b6d9c6ba24a70b7833025aaba41a4ce06a
Gustavo Soares <gustavo10.fsoares@gmail.com>
2018-04-13 14:43:08 -0300

    implementando ModelAbstract

---
+++ server/src/HospitalApi/Model/ModelAbstract.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace HospitalApi\Model;
+
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Tools\Setup;
+
+abstract class ModelAbstract 
+{
+
+	public $em;
+	private $entityPath;
+
+	public function __construct($entityPath) {
+		$this->entityPath = $entityPath;
+		$this->em = $this->createEntityManager();
+	}
+
+	public function createEntityManager() {
+
+		$path = ['HospitalApi\Entity'];
+		$devMode = true;
+
+		$config = Setup::createAnnotationMetadataConfiguration($path, $devMode);
+
+		$connectionOptions = [
+		    'dbname' => 'hospital_api',
+		    'user' => 'root',
+		    'password' => 'root',
+		    'host' => 'localhost',
+		    'driver' => 'pdo_mysql'
+		];
+
+		//Passo 3 - Objeto de persistencia
+		return EntityManager::create($connectionOptions, $config);
+	}
+
+	public function insert($obj) {
+		$this->em->persist($obj);
+		$this->em->flush();
+	}
+
+	public function update($obj) {
+		$this->em->merge($obj);
+		$this->em->flush();
+	}
+
+	public function delete($obj) {
+		$this->em->remove($obj);
+		$this->em->flush();
+	}
+
+	public function findById($id) {
+		return $this->em->find($this->entityPath, $id);
+	}
+
+	public function findAll() {
+		$collection = $this->em->getRepository($this->entityPath)->findAll();
+
+		$data = [ ];
+		foreach ($collection as $obj) {
+			$data[] = $obj;
+		}
+
+		return $data;
+	}
+}
