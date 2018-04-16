cda286447de8883a347c1e1183747a5b6ca625ac
Gustavo Soares <gustavo10.fsoares@gmail.com>
2018-04-16 09:39:08 -0300

    Implementando EntityAbstract

--- server/src/HospitalApi/Entity/EntityAbstract.php
+++ server/src/HospitalApi/Entity/EntityAbstract.php
@@ -4,22 +4,24 @@ namespace HospitalApi\Entity;
 
 abstract class EntityAbstract
 {
-    private $classVars;
+    // private $classVars;
     
     public function __construct() {
-        $className = get_class($this);
-        $this->classVars = get_class_vars($className);
 
-        unset($this->classVars['classVars']);
     }
 
     public function getClassName() {
         return get_class($this);
     }
 
+    public function getClassVars()
+    {
+        return get_class_vars($this->getClassName());
+    }
+
     public function toString(){
         $obj;
-        foreach ($this->classVars as $var => $value) {
+        foreach ($this->getClassVars() as $var => $value) {
             $obj.="[$var:".$this->$var."]";
         }
         return $obj;
@@ -27,7 +29,7 @@ abstract class EntityAbstract
 
     public function toArray(){
         $obj;
-        foreach ($this->classVars as $var => $value) {
+        foreach ($this->getClassVars() as $var => $value) {
             $obj[$var] = $this->$var;
         }
         return $obj;
