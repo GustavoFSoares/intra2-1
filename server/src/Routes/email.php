5637d949deff4a6754f6642137ee3a9126e43f20
Gustavo Soares <gustavo10.fsoares@gmail.com>
2018-04-17 14:22:41 -0300

    corrigindo rota

--- server/src/Routes/email.php
+++ server/src/Routes/email.php
@@ -3,7 +3,7 @@ use Slim\Http\Request;
 use Slim\Http\Response;
 use Doctrine\DBAL\Driver\PDOException;
 
-$app->group('/email', function() {
+$app->group('/mail', function() {
     $this->post('/send', "HospitalApi\Controller\EmailController:buildMailAction", function(Request $req, Response $res, array $args) {
     
 });
