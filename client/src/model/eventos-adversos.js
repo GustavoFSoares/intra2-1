a99642f847d8cd752ff69ebc741bc1fee438e542
Gustavo Soares <gustavo10.fsoares@gmail.com>
2018-04-19 11:18:50 -0300

    options para select

--- client/src/model/eventos-adversos.js
+++ client/src/model/eventos-adversos.js
@@ -17,14 +17,38 @@ export const getSectorsHpsc = () => {
 export const getUnits = () => {
     return [
         { text: 'Hospital Universitário - HU', value: 'hu' },
-        { text: 'Hospital Pronto Socorro - HPSC', value: 'hpsc' } 
+        { text: 'Hospital Pronto Socorro - HPSC', value: 'hpsc' },
+        { text: 'CAPS - Amanhecer', value: 'caps-amanhecer' },
+        { text: 'CAPS - Novo Tempo', value: 'caps-novo-tempo' },
+        { text: 'CAPS - Recanto dos Girassóis', value: 'caps-recanto-dos-girassóis' },
+        { text: 'CAPS - Travessia', value: 'caps-travessia' },
+        { text: 'UPA - Caçapava', value: 'upa-cacapava' },
+        { text: 'UPA - Rio Branco', value: 'upa-riopBranco' },
     ]
 }
 
 export const getEvents = () => {
     return [
-        { text: 'Drepressão Pós-Almoço', value: 'dpa', selected: '' },
-        { text: 'Fome Excessiva Compulsiva', value: 'fm', selected: '' } 
+        { text: 'Desenvolvimento de Lesão por Pressão(UP)', value: 'dpa', selected: '' },
+        { text: 'Erro de Medicação (medicamento errado, paciente errado, medicação não administrada)', value: 'fm', selected: '' },
+        { text: 'Erro de Processo (Paciente deixa de realizar exames e/ou cirurgia)', value: 'fm', selected: '' },
+        { text: 'Falha na Administração de Dietas (Administrada diferente do prescrito, contaminada)', value: 'fm', selected: '' },
+        { text: 'Falha na Identificação do Paciente', value: 'fm', selected: '' },
+        { text: 'Flebite', value: 'fm', selected: '' },
+        { text: 'Fuga do paciente', value: 'fm', selected: '' },
+        { text: 'Hidratação não administrada', value: 'fm', selected: '' },
+        { text: 'Infecção hospitalar', value: 'fm', selected: '' },
+        { text: 'Morte inesperada', value: 'fm', selected: '' },
+        { text: 'Óbito intra-operatório ou pós-hoperatório imediato (paciente eletivos ou com baixo risco cirúrgico', value: 'fm', selected: '' },
+        { text: 'PCR inesperado', value: 'fm', selected: '' },
+        { text: 'Procedimento cirúrgico no lado errado do corpo', value: 'fm', selected: '' },
+        { text: 'Procedimento cirúrgico no paciente errado', value: 'fm', selected: '' },
+        { text: 'Queda', value: 'fm', selected: '' },
+        { text: 'Queixa técnica (Produto/Medicação)', value: 'fm', selected: '' },
+        { text: 'Reação medicamentosa', value: 'fm', selected: '' },
+        { text: 'Reação transfuncional ou imediatamente após transfusão de hemecomponentes', value: 'fm', selected: '' },
+        { text: 'Rótulo mal preenchido, incompleto ou incorreto', value: 'fm', selected: '' },
+        { text: 'Outros', value: 'fm', selected: '' },
     ]
 }
 
