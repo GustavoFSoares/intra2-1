9b62b58910e411ad04b3245346fe1d22a449a5f7
Gustavo Soares <gustavo10.fsoares@gmail.com>
2018-04-20 16:30:51 -0300

    Criando arquivo para pojo's

---
+++ client/src/entity/index.js
@@ -0,0 +1,22 @@
+export const AdverseEventsStory = {
+            mustReturn: '',
+            unit: '',
+            setor: '',
+            event: '',
+            complement: { description: '', conduct: '', },
+            person: { name: '', phonenumber: '', mail: '', },
+            patient: { involved: true, name: "", number: "",
+            }
+        }
+
+export const Mail = {
+            success: {
+                type: "success",
+                text: "Email enviado com Sucesso"
+            },
+            failed: {
+                type: "danger",
+                text: "Email não pode ser enviado. Por favor tente novamente"
+            }
+        }
+    
