d88182b0a55f01aed8828ed74fbcd0589eb841e1
Gustavo Soares <gustavo10.fsoares@gmail.com>
2018-04-24 16:53:02 -0300

    removendo bootstrap-vue e vuex

--- client/src/main.js
+++ client/src/main.js
@@ -7,7 +7,6 @@ import router from './routes'
 import VueTheMask from 'vue-the-mask'
 import VeeValidate, { Validator } from 'vee-validate';
 import translate from 'vee-validate/dist/locale/pt_BR';
-import Vuex from 'vuex'
 
 import 'es6-promise/auto' //testar promise em IE11
 import 'bootstrap/dist/css/bootstrap.css'
@@ -20,8 +19,6 @@ Validator.localize('pt_BR', translate)
 Vue.use(VueTheMask)
 Vue.use(VeeValidate);
 Vue.use(VueResource)
-Vue.use(Vuex)
-// Vue.use(BootstrapVue);
 
 new Vue({
   el: '#app',
