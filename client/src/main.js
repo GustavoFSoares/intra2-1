00e6de3cbe13be29ddb81a2c429870ad96d63b65
Gustavo Soares <gustavo10.fsoares@gmail.com>
2018-04-24 08:26:21 -0300

    adicionando vuex e es6-promise ao projeto

--- client/src/main.js
+++ client/src/main.js
@@ -7,7 +7,9 @@ import router from './routes'
 import VueTheMask from 'vue-the-mask'
 import VeeValidate, { Validator } from 'vee-validate';
 import translate from 'vee-validate/dist/locale/pt_BR';
+import Vuex from 'vuex'
 
+import 'es6-promise/auto' //testar promise em IE11
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/js/bootstrap.js'
 import 'font-awesome/css/font-awesome.css'
@@ -18,6 +20,8 @@ Validator.localize('pt_BR', translate)
 Vue.use(VueTheMask)
 Vue.use(VeeValidate);
 Vue.use(VueResource)
+Vue.use(Vuex)
+// Vue.use(BootstrapVue);
 
 new Vue({
   el: '#app',
