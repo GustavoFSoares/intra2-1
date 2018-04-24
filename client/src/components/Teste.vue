c5fd08d2b5802e425ee18ca8c7efa559cf8dcfa6
Gustavo Soares <gustavo10.fsoares@gmail.com>
2018-04-24 16:56:57 -0300

    Esvaziando componente de teste

--- client/src/components/Teste.vue
+++ client/src/components/Teste.vue
@@ -1,36 +1,12 @@
 <template>
-    <div>
-        <h1>{{welcome}}</h1>
-        <h1>{{message}}</h1>
-        <h1>{{count}}</h1>
-        <button @click="pressed">Click me</button>
-    </div>
+    
 </template>
 
 <script>
-import { modal } from "@/store";
 export default {
-    data() {
-        return {
-            welcome: "OLA MEU CHAPA"
-        }
-    },
-    computed: {
-        message() {
-            return modal.getters.message
-        },
-        count() {
-            return modal.getters.counter
-        }
-    },
-    methods: {
-        pressed(){
-            modal.dispatch('increment', 20)
-        }
-    },
+
 }
 </script>
-
 
 <style scoped>
 
