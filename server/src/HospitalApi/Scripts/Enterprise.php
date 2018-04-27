4f9f5d7d5d258c33adefb9ba9ebfa10ef5a9fec8
Gustavo Soares <gustavo10.fsoares@gmail.com>
2018-04-27 17:26:30 -0300

    Listando Atributos para inserção padrao automatica

--- server/src/HospitalApi/Scripts/Enterprise.php
+++ server/src/HospitalApi/Scripts/Enterprise.php
@@ -5,9 +5,17 @@ use HospitalApi\Model\EnterpriseModel;
 
 $model = new EnterpriseModel();
 
-$enterprises = [ ];
+$enterprises = [
+    [ 'id' => 'hu', 'name' => 'Hospital Universitário - HU' ],
+    [ 'id' => 'hpsc', 'name' => 'Hospital Pronto Socorro - HPSC' ],
+    [ 'id' => 'caps-amanhecer', 'name' => 'CAPS - Amanhecer' ],
+    [ 'id' => 'caps-novo-tempo', 'name' => 'CAPS - Novo Tempo' ],
+    [ 'id' => 'caps-girassois', 'name' => 'CAPS - Recanto dos Girassóis' ],
+    [ 'id' => 'caps-travessia', 'name' => 'CAPS - Travessia' ],
+    [ 'id' => 'upa-rio-branco', 'name' => 'UPA - Rio Branco' ],
+ ];
 
 foreach ($enterprises as $enterprise) {
-    $enterprise = new Enterprise($enterprise['name']);
+    $enterprise = new Enterprise($enterprise['id'], $enterprise['name']);
     $model->insert($enterprise);
 }
