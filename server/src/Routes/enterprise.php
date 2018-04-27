c9509230024359071cac33c881fe23804038ebb6
Gustavo Soares <gustavo10.fsoares@gmail.com>
2018-04-27 17:25:51 -0300

    declarando Rota de busca para Empresa Evento e Setor

---
+++ server/src/Routes/enterprise.php
@@ -0,0 +1,23 @@
+<?php
+use Slim\Http\Request;
+use Slim\Http\Response;
+use Doctrine\DBAL\Driver\PDOException;
+
+/**
+ * @api @group
+ * <b>Rotas de Links</b>
+ * Grupo de Rotas relacionadas a email
+ * Indentificadas pelo caminho <i>/mail</i>
+ */
+$app->group('/enterprise', function () {
+
+    /**
+     * <i>Getter para Links</i>
+     * Recebe um Id(optionalmente) e retorna um objeto
+     * com as Links de Atalho cadastrados no Banco de Dados
+     * @param Optional $id
+     * @return Json Rotas 
+     */
+    $this->get('/[{id}]', "HospitalApi\Controller\EnterpriseController:get", function (Request $req, Response $res, array $args) { });
+
+});
