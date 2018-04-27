9032463f68b729b79adb99337b5238f0c60a1545
Gustavo Soares <gustavo10.fsoares@gmail.com>
2018-04-27 17:28:11 -0300

    Criando Model para Setor

--- server/src/HospitalApi/Model/SectorModel.php
+++ server/src/HospitalApi/Model/SectorModel.php
@@ -2,17 +2,29 @@
 namespace HospitalApi\Model;
 
 use HospitalApi\Entity\Sector;
+use HospitalApi\Entity\BossSector;
+use HospitalApi\Entity\Enterprise;
 
 class SectorModel extends ModelAbstract
 {
 
     public $entity;
 
-    public function __construct()
-    {
+    public function __construct() {
+        $enterprise = new Enterprise();
+        $bossSector = new BossSector();
 
-        $this->entity = new Sector();
+        $this->entity = new Sector($enterprise, $bossSector);
         parent::__construct();
     }
+
+    public function findSectorsByEnterprise($id = null) {
+        $repository = $this->em->getRepository("HospitalApi\Entity\Sector");
+        
+        $collection = $repository->findBy(['Enterprise' => $id]);
+        return $collection;
+    }
+
+
 
 }
