d2f43208956a40c037051cb5c17e1ee30c00718c
Gustavo Soares <gustavo10.fsoares@gmail.com>
2018-05-02 09:56:07 -0300

    criado template para email Eventos Adversos

---
+++ client/src/model/email_template/adverse-events-template.js
@@ -0,0 +1,58 @@
+export default (report) => {
+    
+    let email = {}
+    email.subject = `Evento Adverso - ${report.event}`
+    email.receiver = `gustavo.soares@gampcanoas.com.br`
+    email.sender = report.sender
+
+
+    email.body = `
+        <fieldset> <legend>Colaborador:</legend>`
+
+    if (report.sender.anonymous) {
+        email.body += `<b>Este relato foi enviado anonimamente</b><br>`
+    }
+
+
+    email.body += `
+            Nome: ${report.sender.name}<br>
+            Telefone: ${report.sender.phonenumber}<br>
+            E-mail: ${report.sender.mail}<br>
+        </fieldset><br>
+
+        <fieldset>
+            <legend>Local do Evento:</legend>
+            Unidade: `+ report.enterprise + `<br>
+            Setor: Código `+ report.sector+ `<br>
+        </fieldset><br>
+
+        <fieldset>
+            <legend>Descrição Evento:</legend>
+            Evento: Código ${report.event}<br>
+            Descrição: ${report.complement.description}<br>
+            Coonduta Aplicada: ${report.complement.conduct}<br>
+               
+            <br>
+
+            <fieldset>
+                <legend>Paciente Envolvido:</legend>`
+    if (!report.patient.involved) {
+        email.body += `<b>Não houve problemas com paciente</b><br>`
+    } else {
+
+        email.body += `
+                        Nome: ${report.patient.nome}<br>
+                        Número do Atendimento: ${report.patient.number}<br>`
+    }
+    email.body += `
+            </fieldset>
+        </fieldset>
+                
+        <br>`
+
+    if (report.mustReturn) {
+        email.body += `<b>Por favor, me responda sobre este email</b><br>`
+    }
+    
+    return email
+}
