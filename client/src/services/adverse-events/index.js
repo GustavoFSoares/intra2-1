df0469a7cc65dbcae319d73bfe6b27a5d202e038
Gustavo Soares <gustavo10.fsoares@gmail.com>
2018-05-02 09:55:46 -0300

    criando métodos para buscar descrição dos tipos de eventos

--- client/src/services/adverse-events/index.js
+++ client/src/services/adverse-events/index.js
@@ -8,22 +8,41 @@ const saveData = (mail) => {
     return http.post(`/adverse-events/save`, mail).then( res => res.data ) 
 }
 
+// =================================================================================
 const getEvents = () => {
-    return http.get(`/event/`).then( res => res.data )
+    return http.get(`/event/`).then(res => res.data)
 }
 
 const getEnterprises = () => {
-    return http.get(`/enterprise/`).then( res => res.data )
+    return http.get(`/enterprise/`).then(res => res.data)
 }
 
 const getSectorsBy = (id) => {
-    return http.get(`/sector/enterprise/${id}`).then( res => res.data )
+    return http.get(`/sector/enterprise/${id}`).then(res => res.data)
+}
+// =================================================================================
+const getEventById = (id) => {
+    return http.get(`/event/${id}`).then(res => res.data )
 }
 
+const getEnterpriseById = (id) => {
+    return http.get(`/enterprise/${id}`).then(res => res.data)
+}
+
+const getSectorById = (id) => {
+    return http.get(`/sector/${id}`).then(res => res.data)
+}
+// =================================================================================
 export default {
     sendMail,
     saveData,
     getEvents,
     getEnterprises,
     getSectorsBy,
+}
+
+export const getters = {
+    getEventById,
+    getEnterpriseById,
+    getSectorById,
 }
