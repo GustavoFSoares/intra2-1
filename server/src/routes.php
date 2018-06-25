888bbbcbbce0f13a1257d3cc3ec8d4c130c49053
Gustavo Soares <gustavo10.fsoares@gmail.com>
2018-06-25 11:35:01 -0300

    movendo rotas para pasta interna do projeto

--- server/src/routes.php
+++ server/src/routes.php
@@ -9,22 +9,10 @@ $app->add(function ($req, $res, $next) {
         ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
         ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
 });
-
-$path = __dir__ . '/Routes';
+$path = __dir__ . '/HospitalApi/Routes';
 $dir = dir($path);
 while ($file = $dir->read()) {
     if($file != '.' && $file != '..'){
         require "$path/$file";
     }
 }
-
-$app->get('/script/insert', function(Request $req, Response $res, array $args){
-        require __DIR__ . "/HospitalApi/Scripts/index.php";
-});
-
-$app->get('/insert/{file}', function (Request $req, Response $res, array $args) {
-        if (isset($args['file'])) {
-                $file = $args['file'];
-                require __DIR__ . "/HospitalApi/Scripts/$file.php";
-        }
-});
