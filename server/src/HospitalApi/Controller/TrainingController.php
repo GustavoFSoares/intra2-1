23643c44e70512c5b83ab744f9e18ad814bbc107
Gustavo Ferreira Soares <gustavo.soares@maededeus.com.br>
2018-07-19 09:21:39 -0300

    criando metodo recursivo para converter Resultado do Banco em Array

---
+++ server/src/HospitalApi/Controller/TrainingController.php
@@ -0,0 +1,62 @@
+<?php
+namespace HospitalApi\Controller;
+
+use HospitalApi\Model\TrainingModel;
+
+/**
+ * <b>TrainingController</b>
+ */
+class TrainingController extends ControllerAbstract
+{
+    public function __construct() {
+        parent::__construct(new TrainingModel());
+    }
+
+    public function insert($req, $res, $args){
+        $values = (object)$req->getParsedBody();
+        
+        $model = $this->getModel();
+        
+        $entity = $model->mounte($values);
+        $model->doInsert($entity);
+        
+        return $res->withJson(true);
+    }
+
+    public function translateCollection($entity, $arr = []) {
+        if($entity) {
+            if(is_array($entity)){
+                foreach ($entity as $key => $value) {
+                    $arr[$key] = $this->translateCollection($value);
+                }
+            } else {
+                foreach ($entity->toArray() as $key => $value) {
+                    
+                    if($value instanceof \HospitalApi\Entity\EntityAbstract){
+                        $arr[$key] = $this->translateCollection($value);
+        
+                    } else {
+                        $method = "get$key";
+        
+                        if(method_exists($entity, $method)) {
+                            $result = $entity->$method();
+        
+                            if(method_exists($result, "toArray")) {
+        
+                                foreach ($result->toArray() as $k => $val) {
+                                    $arr[$key][$k] = $this->translateCollection($val);
+                                }
+                                
+                            } else {
+                                $arr[$key] = $value;
+                            }
+                        }
+                    }
+        
+                }
+                
+            }
+        }
+        return $arr;
+    }
+}
