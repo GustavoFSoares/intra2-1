7126b208378d345485d34324153aebef74470997
Gustavo Ferreira Soares <gustavo.soares@maededeus.com.br>
2018-08-14 16:45:37 -0300

    criando componente para YesNo custumizados e Confirm

---
+++ client/src/components/shared/Alert.js
@@ -0,0 +1,34 @@
+const bootbox = require('bootbox');
+
+export default {
+    YesNo: (title = "YesNo", message = "Message") => {
+        return new Promise((resolve) => {
+            bootbox.confirm({
+                title: title,
+                message: message,
+                buttons: {
+                    cancel: {
+                        label: 'Não',
+                        className: 'btn-danger'
+                    },
+                    confirm: {
+                        label: 'Sim',
+                        className: 'btn-success'
+                    },
+
+                },
+                callback: result => {
+                    resolve(result)
+                }
+            })
+        })
+    },
+    Confirm: (message = "Message") => {
+        return new Promise((resolve) => {
+            bootbox.confirm(
+                message,
+                result => { resolve(result) }
+            )
+        })
+    }
+}
