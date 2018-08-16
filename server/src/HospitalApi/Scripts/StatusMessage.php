c8969a57ef51a6433acf4a7606342db593583b18
Gustavo Ferreira Soares <gustavo.soares@maededeus.com.br>
2018-08-16 15:54:20 -0300

    atualizando lista de mensagem de status

--- server/src/HospitalApi/Scripts/StatusMessage.php
+++ server/src/HospitalApi/Scripts/StatusMessage.php
@@ -4,9 +4,9 @@ use HospitalApi\Entity\StatusMessage;
 use HospitalApi\Model\StatusMessageModel;
 
 $model = new StatusMessageModel();
+echo "Atualizando Mensagem de Status\n";
 
-if(!$model->findAll()){
-    $StatusMessages = [
+    $statusMessages = [
         ['id' => 'user_incorrect', 'status' => false, 'message'=> 'Usuário ou Senha incorretos.', 'type' => 'danger'],        
         ['id' => 'user_inactive', 'status' => false, 'message' => 'Seu Usuário foi inativado, por favor contate a TI', 'type' => 'warning'],
         ['id' => 'group_not_found', 'status' => false, 'message' => 'Você não possui grupo cadastrado. Contate a TI', 'type' => 'warning'],
@@ -14,16 +14,19 @@ if(!$model->findAll()){
         ['id' => 'in_training', 'status' => false, 'message' => 'Já cadastrado nesse treinamento', 'type' => 'warning'],
     ];
     
-    foreach ($StatusMessages as $statusMessage) {
-        $StatusMessage = new StatusMessage();
-        $StatusMessage
-            ->setId($statusMessage['id'])
-            ->setstatus($statusMessage['status'])
-            ->setMessage($statusMessage['message'])
-            ->setType($statusMessage['type']);
-        $model->doInsert($StatusMessage);
+    foreach ($statusMessages as $statusMessage) {
+        if(!$model->getRepository()->findById($statusMessage['id'])){
+            $StatusMessage = new StatusMessage();
+            $StatusMessage
+                ->setId($statusMessage['id'])
+                ->setstatus($statusMessage['status'])
+                ->setMessage($statusMessage['message'])
+                ->setType($statusMessage['type']);
+            $model->doInsert($StatusMessage);
+
+            echo "      Mensagem de Status ".$statusMessage['id']." INSERTED\n";
+        } else {
+            echo "      Mensagem de Status ".$statusMessage['id']." ignored\n";
+        }
+
     }
-    echo "StatusMessage Inserted\n";
-} else {
-    echo "StatusMessage was ignored\n";
-}
