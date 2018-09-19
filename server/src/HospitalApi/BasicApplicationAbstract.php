41715726df1ed49f2aa20a759307ec4e35494a0f
Gustavo Ferreira Soares <gustavo.soares@maededeus.com.br>
2018-09-19 09:06:56 -0300

    adicionando email padrao para aplicação

--- server/src/HospitalApi/BasicApplicationAbstract.php
+++ server/src/HospitalApi/BasicApplicationAbstract.php
@@ -8,6 +8,15 @@ class BasicApplicationAbstract
         return DEBUG_ACTIVE;
     }
 
+    public function getEmailDefault(){
+        return [
+            'name' => EMAIL_DEFAULT_NAME,
+            'email' => EMAIL_DEFAULT_EMAIL,
+            'password' => EMAIL_DEFAULT_PASSWORD,
+            'host' => EMAIL_DEFAULT_HOST,
+        ];
+    }
+    
     public function getDebugEmail(){
         return DEBUG_EMAIL;
     }
