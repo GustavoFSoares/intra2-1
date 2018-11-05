f048a8fa13299ea92d93c82cfad9cc529f301fcf
Gustavo Ferreira Soares <gustavo.soares@maededeus.com.br>
2018-11-05 12:59:31 -0200

    Adicionando Bootstrap UIV ao projeto

--- client/src/main.js
+++ client/src/main.js
@@ -4,19 +4,21 @@ import App from './App'
 import VueResource from 'vue-resource'
 import router from './routes'
 
+import * as uiv from 'uiv'
 import VueSession from 'vue-session'
 import VueTheMask from 'vue-the-mask'
 import VeeValidate, { Validator } from 'vee-validate';
+import Tooltip from 'vue-directive-tooltip';
+
+import locale from 'uiv/src/locale/lang/pt-BR'
 import translate from 'vee-validate/dist/locale/pt_BR';
 
-import Tooltip from 'vue-directive-tooltip';
-import 'vue-directive-tooltip/css/index.css';
-import '@/../static/directives-styles/VTooltip.css';
-
-Vue.use(Tooltip);
 import 'bootstrap/dist/css/bootstrap.css'
+import 'glyphicons-only-bootstrap/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.js'
 import 'font-awesome/css/font-awesome.css'
+import 'vue-directive-tooltip/css/index.css';
+import '@/../static/directives-styles/VTooltip.css';
 
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fas } from '@fortawesome/free-solid-svg-icons'
@@ -28,9 +30,11 @@ Vue.component('icon', FontAwesomeIcon)
 Vue.config.productionTip = false
 
 Validator.localize('pt_BR', translate)
+Vue.use(uiv, { prefix: 'uiv', locale })
 Vue.use(VueResource)
 Vue.use(VueSession)
 Vue.use(VueTheMask)
+Vue.use(Tooltip);
 Vue.use(VeeValidate);
 
 new Vue({
