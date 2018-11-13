5b40cb02b8c8e9e7ba704f57387c4965fc551760
Gustavo Ferreira Soares <gustavo.soares@maededeus.com.br>
2018-11-13 15:29:15 -0200

    Tela funcionando

--- client/src/components/shared/DirTree.vue
+++ client/src/components/shared/DirTree.vue
@@ -1,49 +1,18 @@
 <template>
-    <div class="container">
-        <!-- <div v-for="(file, index) in folder" :key="index">
-            <div v-if="file.dir">
-                <h2 @click="selected = file.name" :class="{'text-danger': folder[index][selected]}">{{file.name}}</h2>
-                <br/>
-                <uiv-collapse :class="{'show':folder[index][selected]}" v-model="folder[index][selected]">
-                    <dir-tree :folder="file[file.name]"/>
-                </uiv-collapse>
-            </div>
-            <div v-else>
-                {{file.name}}
-            </div>
+    <div class="container" :id="`${componentKey}table`">
+        <div v-for="(file, index) in folder" :key="`${componentKey}-${index}`">
+                <router-link to="" :id="`#label-${index}-${componentKey}`" class="list-group-item list-group-item-action text-left" data-toggle="collapse" :data-target="`#id-${componentKey}-${index}`" aria-expanded="true" :aria-controls="`id-${componentKey}-${index}`" v-if="file.dir">
+                    {{ file.name }}
+                </router-link>
 
-        </div> -->
-        <div :id="`${componentKey}acordeon`" class="list-group">
-            <div v-for="(file, index) in folder" :key="index">
-                <div v-if="file.dir">
-                    {{file.name}}
-                </div>
-                <div>
+                <div :id="`id-${componentKey}-${index}`" class="collapse" v-bind:class="{'show': !file.dir}" :aria-labelledby="`#label-${index}-${componentKey}`" :data-parent="`#${componentKey}table`">
                     <div v-if="file.dir">
-                        <dir-tree :folder="file[file.name]" :componentKey="`${componentKey}${index}`" v-if="file.dir"/>
+                        <dir-tree :folder="file[file.name]" :componentKey="`${componentKey}${index}`"/>
                     </div>
-                    <div>
-                        <p>{{file.name}} <span class="text-danger">(FILE)</span></p>
+                    <div v-else class="text-right">
+                        <p class="text-primary btn-link">{{file.name}}</p>
                     </div>
                 </div>
-                <!-- <div id="index">
-                    <router-link to="" class="list-group-item list-group-item-action" data-toggle="collapse" :data-target="`#id-${componentKey}-${index}-${file.name}`" aria-expanded="true" :aria-controls="`id-${componentKey}-${index}-${file.name}`" v-if="file.dir">
-                        {{ file.name }}
-                    </router-link>
-                </div>
-
-                <div :id="`id-${componentKey}-${index}-${file.name}`" class="collapse" v-bind:class="{'show': file.dir != true }" aria-labelledby="index" :data-parent="`#${componentKey}acordeon`">
-                    <div>
-                        
-                        <div>
-                            <dir-tree :folder="file[file.name]" :componentKey="`${componentKey}${index}`" v-if="file.dir"/>
-                            <div v-else>
-                                {{file.name}}
-                            </div>
-                        </div>
-                    </div>
-                </div> -->
-            </div>
         </div>
     </div>
 </template>
@@ -58,7 +27,7 @@ export default {
     },
     props: {
         folder: '',
-        componentKey: { default: '0' }
+        componentKey: { default: '1' }
     },
     computed: { },
     mounted() {
