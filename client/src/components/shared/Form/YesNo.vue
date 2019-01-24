57184855d79e50be1940298f74ac18d6a16b2249
Gustavo Ferreira Soares <gustavo.soares@maededeus.com.br>
2019-01-24 16:36:23 -0200

    Criando botão SimNao

---
+++ client/src/components/shared/Form/YesNo.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="btn-group btn-group-toggle" data-toggle="buttons">
+        <label class="btn btn-outline-success" v-bind:class="{ 'active' : data == true }" @click="data = true">
+            <input type="radio" name="options"> Sim
+        </label>
+        <label class="btn btn-outline-danger" v-bind:class="{ 'active' : data == false }" @click="data = false">
+            <input type="radio" name="options"> Não
+        </label>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            data: null
+        }
+    },
+    props: ['value'],
+    watch: {
+        value() {
+            this.data = this.value
+        },
+        data() {
+            this.$emit('input', this.data)
+            this.$emit('click')
+        }
+    },
+    mounted() {
+        this.data = this.value
+    },
+}
+</script>
