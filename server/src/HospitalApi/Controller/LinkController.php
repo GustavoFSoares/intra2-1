358b9de3c0e6063047ad224b10073bfd02969d60
unknown <gustavo.soares@HU-APINTRANET.hmd.local>
2019-03-28 17:08:25 -0300

    corrigindo erros do salvamento dos links

--- server/src/HospitalApi/Controller/LinkController.php
+++ server/src/HospitalApi/Controller/LinkController.php
@@ -19,51 +19,61 @@ class LinkController extends ControllerAbstract
 
 		if(array_key_exists('file', $files)) {
 			$file = $files['file'];
-            
-            $destiny = PATH."../../../client/";
-            if( $this->isProduction() ) {
-                $destiny .= "dist/";
-            }
-            $destiny .= "static/img/links/";
-            
-			if($prefix) {
-				$destiny .= "$prefix/";
-			}
 
-			if( !is_dir($destiny) ) {
-				mkdir($destiny);
-			}
+			$fileName = $this->_getFileName($file, $name);
+			$this->_moveToDestiny($file, $prefix, $fileName);
 			
-			$name = $name ? $name : $file->getClientFilename();
-			preg_match_all('/(\.\w{3})$/m', $name, $matches, PREG_SET_ORDER, 0);
-			if( isset($matches[0]) ) {
-				$fileName = $name;
-			} else {
-				$extension = pathinfo($file->getClientFilename(), PATHINFO_EXTENSION);
-				$fileName = "$name.$extension";			
-			}
-			$fileArray = explode('.', $fileName);
-			$name = \Helper\SlugHelper::get( $fileArray[0] );
-			$extension = $fileArray[1];
-			
-			$fileName = "$name.$extension";
-			$destiny .= $fileName;
-			
-			$file->moveTo($destiny);
-			$response = $destiny;
-
+			$response = $fileName;
+		
 		} else {
 			$res->withCode(401);
 			$response = "File Not Found";
 		}
 
-		return $res->withJson($fileName);
+		return $res->withJson($response);
     }
     
     public function changeStatusAction($req, $res, $args) {
         $id = $req->getParam('id');
 
         return $this->getModel()->changeStatus($id);
-    }
+	}
+	
+	private function _getFileName($file, $fileName) {
+		$name = $fileName ? $fileName : $file->getClientFilename();
+		preg_match_all('/(\.\w{3})$/m', $name, $matches, PREG_SET_ORDER, 0);
+		if( isset($matches[0]) ) {
+			$fileName = $name;
+		} else {
+			$extension = pathinfo($file->getClientFilename(), PATHINFO_EXTENSION);
+			$fileName = "$name.$extension";			
+		}
+		$fileArray = explode('.', $fileName);
+		$name = \Helper\SlugHelper::get( $fileArray[0] );
+		$extension = $fileArray[1];
+		$fileName = "$name.$extension";
+		return $fileName;
+	}
+
+	private function _moveToDestiny($file, $prefix, $fileName) {
+		$clientPath = PATH."../../../client";
+        $imagePath = "static/img/links/";
+
+		if($prefix) {
+			$imagePath .= "$prefix/";
+		}
+		$imagePath .= $fileName;
+		
+		$destiny = "$clientPath/$imagePath";
+		$file->moveTo("$destiny");
+
+		if( $this->isProduction() ) {
+			$savedImagePath = $destiny;
+			$destiny = "$clientPath/dist/$imagePath";
+			copy($savedImagePath, $destiny);
+		}
+
+		return $destiny;
+	}
 
 }
